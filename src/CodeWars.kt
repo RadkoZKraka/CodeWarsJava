3b53a5aea05dd0a4878a1f4b26d67be5c7ee6c3c
RadkoZKraka <99608797+RadkoZKraka@users.noreply.github.com>
2022-10-16 15:52:18 +0200

    Initial commit

--- src/CodeWars.kt
+++ src/CodeWars.kt
@@ -1,2 +1,8 @@
-class CodeWars {
-}
+    fun main(){
+        println(getAge("8 Years old"))
+    }
+    fun getAge(yearsOld: String): Int {
+
+        // your code here
+        return yearsOld[0].digitToInt()
+    }
